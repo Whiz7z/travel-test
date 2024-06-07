a4dade179186dc4fd81be97359f7782550821355
Whiz7z <gjevstratov@gmail.com>
2024-06-07 18:42:58 +0100

    finish 15

--- src/itemsStore/items.js
+++ src/itemsStore/items.js
@@ -140,7 +140,7 @@ export const southHighlandsInfo = {
   title: "South & Highlands",
   tags: ["Geysers", "Waterfalls", "Crater lakes"],
   description:
-    "TThe South & Highlands of Iceland offer a breathtaking landscape of diverse natural wonders, from expansive glaciers and active volcanoes to serene waterfalls and black sand beaches. This region is also a haven for adventurers, featuring numerous hiking trails, hidden hot springs, and vast lava fields waiting to be explored. Visitors will be captivated by the blend of tranquility and raw, untamed beauty that defines this unique part of Iceland.",
+    "The South & Highlands of Iceland offer a breathtaking landscape of diverse natural wonders, from expansive glaciers and active volcanoes to serene waterfalls and black sand beaches. This region is also a haven for adventurers, featuring numerous hiking trails, hidden hot springs, and vast lava fields waiting to be explored. Visitors will be captivated by the blend of tranquility and raw, untamed beauty that defines this unique part of Iceland.",
 };
 
 export const southHighlandsighlights = [
